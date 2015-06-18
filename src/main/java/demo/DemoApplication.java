ca3d4cb3abd549758a99b3da60d5b5fa02c0f65f
Dan Wiechert <dan.wiechert@cerner.com>
2015-06-18 13:26:16 -0500

    Added customer repository and REST endpoints.

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    public static void main(String[] args) {
-    	final SpringApplication app = new SpringApplication(DemoApplication.class);
-    	app.setBanner(new KTBanner());
-    	app.run(args);
-//        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		final SpringApplication app = new SpringApplication(
+				DemoApplication.class);
+		app.setBanner(new KTBanner());
+		app.run(args);
+		// SpringApplication.run(DemoApplication.class, args);
+	}
 }
--- src/main/java/demo/controllers/SampleController.java
+++ src/main/java/demo/controllers/SampleController.java
@@ -1,14 +1,38 @@
 package demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.Lists;
+
+import demo.entities.Customer;
+import demo.repositories.CustomerRepository;
+
 @RestController
 public class SampleController {
+	@Autowired
+	CustomerRepository repository;
+	
 	@RequestMapping("/")
 	@ResponseBody
 	public String home() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/allCustomers")
+	@ResponseBody
+	public List<Customer> allCustomers() {
+		return Lists.newArrayList(repository.findAll());
+	}
+	
+	@RequestMapping("/customer")
+	@ResponseBody
+	public List<Customer> customerLastName(@RequestParam(value = "lastName") final String lastName) {
+		return repository.findByLastName(lastName);
 	}
 }
