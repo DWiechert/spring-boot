234ee0566ee0175a05fbeb6282749a83f8866195
Dan Wiechert <dan.wiechert@cerner.com>
2015-06-18 14:38:07 -0500

    Added more query methods.

--- src/main/java/demo/repositories/CustomerRepository.java
+++ src/main/java/demo/repositories/CustomerRepository.java
@@ -7,5 +7,9 @@ import org.springframework.data.repository.CrudRepository;
 import demo.entities.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	List<Customer> findByFirstName(String firstName);
+	
 	List<Customer> findByLastName(String lastName);
+	
+	List<Customer> findByFirstNameAndLastName(String firstName, String lastName);
 }
