91e8cc7e892ceae7e38ac0c42a08ae244b9b2984
Dan Wiechert <dan.wiechert@cerner.com>
2015-06-18 11:00:52 -0500

    Removed unnecessary annotation.

--- src/main/java/demo/controllers/SampleController.java
+++ src/main/java/demo/controllers/SampleController.java
@@ -1,12 +1,10 @@
 package demo.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 public class SampleController {
 	@RequestMapping("/")
 	@ResponseBody
